fb47966f48b6eb3a45a141d139a6644378ec7c18
OMEO <lyk082401@qq.com>
2022-04-10 23:13:58 +0800

    Update code files

--- queslib/res/js/web.catalog.js
+++ queslib/res/js/web.catalog.js
@@ -1029,12 +1029,12 @@ catalog.init = function($select)
 {
 	try
 	{
-		(/(iPhone|iPad|iPod|iOS)/i).test(navigator.userAgent) && ($select.style.display = "none") && alert("网站未适配苹果浏览器，如遇问题请反馈给开发者！");
-		((!+[1,]) || self.ActiveXObject || self.attachEvent) && alert("不推荐使用IE浏览器访问，如遇问题请更换其他浏览器！");
+		(/(iPhone|iPad|iPod|iOS)/i).test(navigator.userAgent) && ($select.style.display = "none") && ($.Zebra_Dialog || alert)("网站未适配苹果浏览器，如遇问题请反馈给开发者！");
+		((!+[1,]) || self.ActiveXObject || self.attachEvent) && ($.Zebra_Dialog || alert)("不推荐使用IE浏览器访问，如遇问题请更换其他浏览器！");
 	}
 	catch(e)
 	{
-		alert(e.stack);
+		console.warn(e);
 	}
 	// 清空选项（包括空白节点）
 	while($select.childNodes.length)
