c2930ebe0bc1d612d186ad202ca1e2fefd81d5c5
OMEO <lyk082401@qq.com>
2022-04-10 23:00:23 +0800

    Update code files

--- queslib/res/js/web.catalog.js
+++ queslib/res/js/web.catalog.js
@@ -1029,7 +1029,7 @@ catalog.init = function($select)
 {
 	try
 	{
-		(/(iPhone|iPad|iPod|iOS)/i).test(navigator.userAgent) && (document.title = "网站未适配苹果浏览器，如遇问题请反馈给开发者！", $select.style.display = "none");
+		(/(iPhone|iPad|iPod|iOS)/i).test(navigator.userAgent) && ($select.style.display = "none") && alert("网站未适配苹果浏览器，如遇问题请反馈给开发者！");
 		((!+[1,]) || self.ActiveXObject || self.attachEvent) && alert("不推荐使用IE浏览器访问，如遇问题请更换其他浏览器！");
 	}
 	catch(e)
