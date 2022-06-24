2d9c081c6ead688f35dfa1f40b6e9dd522ec7b65
OMEO <lyk082401@qq.com>
2022-06-25 00:08:43 +0800

    Update code files

--- queslib/res/js/web.hookConsoleLogs.js
+++ queslib/res/js/web.hookConsoleLogs.js
@@ -1,57 +1,57 @@
-(function($console)
-{
-	try
-	{
-		let moments = function($time = +(new Date()))
+		(function($console)
 		{
-			let date = new Date($time - (new Date()).getTimezoneOffset() * 60 * 1000);
-			return date.toJSON().slice(0, 19).replace("T", " ").concat(" ").concat(String(date.getTime()).slice(-3));
-		};
-		if($console)
-		{
-			$console.moments = moments;
-			Object.entries(
+			try
 			{
-				log: $console.log,
-				info: $console.info,
-				warn: $console.warn,
-				error: $console.error,
-				debug: $console.debug
-			})
-			.forEach(function($kv, $i, $all)
-			{
-				if($kv[1] && $kv[1].apply)
+				let moments = function($time = +(new Date()))
 				{
-					$console[$kv[0]] = function()
+					let date = new Date($time - (new Date()).getTimezoneOffset() * 60 * 1000);
+					return date.toJSON().slice(0, 19).replace("T", " ").concat(" ").concat(String(date.getTime()).slice(-3));
+				};
+				if($console)
+				{
+					$console.moments = moments;
+					Object.entries(
 					{
-						try
+						log: $console.log,
+						info: $console.info,
+						warn: $console.warn,
+						error: $console.error,
+						debug: $console.debug
+					})
+					.forEach(function($kv, $i, $all)
+					{
+						if($kv[1] && $kv[1].apply)
 						{
-							let param1 = ["[" + $kv[0].toUpperCase() + "][" + moments().split(/-\d+\s+/)[1] + "][" + String(arguments.length) + "]", new Error($kv[0].toUpperCase())];
-							let param2 = Array.prototype.slice.call(arguments);
-							let params = param1.concat(["\n\t"]).concat(param2);
-							// console.table(webHookConsoleLogs);
-							(window.webHookConsoleLogs || (window.webHookConsoleLogs = [])).push(params);
-							if(window.eruda && eruda.get && eruda.get("console") && eruda.get("console")[$kv[0]])
+							$console[$kv[0]] = function()
 							{
-								if(eruda.get("console")[$kv[0]] != $console[$kv[0]])
+								try
 								{
-									// eruda.get("console")[$kv[0]](param1, param2);
-									// return;
+									let param1 = ["[" + $kv[0].toUpperCase() + "][" + moments().split(/-\d+\s+/)[1] + "][" + String(arguments.length) + "]", new Error($kv[0].toUpperCase())];
+									let param2 = Array.prototype.slice.call(arguments);
+									let params = param1.concat(["\n\t"]).concat(param2);
+									// console.table(webHookConsoleLogs);
+									(window.webHookConsoleLogs || (window.webHookConsoleLogs = [])).push(params);
+									if(window.eruda && eruda.get && eruda.get("console") && eruda.get("console")[$kv[0]])
+									{
+										if(eruda.get("console")[$kv[0]] != $console[$kv[0]])
+										{
+											// eruda.get("console")[$kv[0]](param1, param2);
+											// return;
+										}
+									}
+									$kv[1].apply($console, [param1, param2]);
 								}
-							}
-							$kv[1].apply($console, [param1, param2]);
+								catch(e)
+								{
+									window.alert && alert(e.stack || e.message);
+								}
+							};
 						}
-						catch(e)
-						{
-							window.alert && alert(e.stack || e.message);
-						}
-					};
+					});
 				}
-			});
-		}
-	}
-	catch(e)
-	{
-		window.alert && alert(e.stack || e.message);
-	}
-})(window.console);
+			}
+			catch(e)
+			{
+				window.alert && alert(e.stack || e.message);
+			}
+		})(window.console);
